aadb7eb554dedf288e5750468ffcc87bb46a9fc8
nguyenzung <nguyenvietdungcs52@gmail.com>
2023-04-07 23:55:43 +0700

    clean code

--- HttpConnector.sol
+++ HttpConnector.sol
@@ -8,13 +8,11 @@ contract RestConnector {
 
     function call(string memory url) public returns (string memory out) {
         assembly {
-            let ptr := mload(0x40)
-            let success := call(gas(), 0x20, 0x0, url, 0x100, ptr, 0x100)
+            out := mload(0x40)
+            let success := call(gas(), 0x20, 0x0, url, 0x100, out, 0x100)
             switch success
             case 0 {
                 revert(0,0)
-            } default {
-                out := ptr
             }
         }
         data = out;
